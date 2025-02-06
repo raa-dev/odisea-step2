09178926e90e7cde0266988ed3820421dd37c848
rodrigo ramos xochiteotzin <rodrx20@gmail.com>
2025-02-05 22:17:25 -0600

    header updated

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { NextPage } from "next";
-import { ChatLayout } from "~~/components/layouts/ChatLayout";
+// import { ChatLayout } from "~~/components/layouts/ChatLayout";
 import { CollectedLayout } from "~~/components/layouts/CollectedLayout";
 
 const Home: NextPage = () => {
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Step 2</span>
           </h1>
-          <ChatLayout />
+          {/* <ChatLayout /> */}
         </div>
         <CollectedLayout />
       </section>
